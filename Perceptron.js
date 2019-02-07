aa26ccf481b8d00616f925116f6ba1a728a0a73e
Roger Louisse Eustaquio <roger@simplexi.com.ph>
2019-02-07 08:20:22 +0800

    added bias

--- Perceptron.js
+++ Perceptron.js
@@ -4,6 +4,7 @@ class Perceptron {
         for (let i = 0; i < inputCount; i++) {
             this.weights[i] = random(-1, 1);
         }
+        this.bias = random(-1, 1);
         this.lr = 0.0001;
     }
 
@@ -12,6 +13,7 @@ class Perceptron {
         for (let i = 0; i < inputs.length; i++) {
             sum += inputs[i] * this.weights[i];
         }
+        sum += this.bias;
         return sign(sum);
     }
 
@@ -21,18 +23,19 @@ class Perceptron {
         for (let i = 0; i < this.weights.length; i++) {
             this.weights[i] += error * inputs[i] * this.lr;
         }
+        this.bias += error * this.lr;
     }
 
     show() {
         let x1 = -1;
         let x2 = 1;
-        let y1 = (-(this.weights[0] * x1) - this.weights[2]) / this.weights[1];
-        let y2 = (-(this.weights[0] * x2) - this.weights[2]) / this.weights[1];
+        let y1 = (-(this.weights[0] * x1) - this.bias) / this.weights[1];
+        let y2 = (-(this.weights[0] * x2) - this.bias) / this.weights[1];
         x1 = 0;
         x2 = width;
         y1 = map(y1, -1, 1, height, 0);
         y2 = map(y2, -1, 1, height, 0);
-        stroke(0, 0, 200);
+        stroke(0, 200, 200);
         strokeWeight(3);
         line(x1, y1, x2, y2);
     }
--- Point.js
+++ Point.js
@@ -2,7 +2,7 @@ class Point {
     constructor(x, y) {
         this.x = typeof x === 'undefined' ? random(-1, 1) : x;
         this.y = typeof y === 'undefined' ? random(-0.5, 0.5) : y;
-        this.train_data = [this.x, this.y, 1];
+        this.train_data = [this.x, this.y];
         this.label = this.y > lineEquation(this.x) ? 1 : -1;
         this.color = this.label === 1 ? 255 : 0;
     }
